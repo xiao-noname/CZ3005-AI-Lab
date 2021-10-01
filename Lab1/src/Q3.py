16b4ed19831c16ad26010b0326f4fc870ae3e460
quacklinhui <61533311+quacklinhui@users.noreply.github.com>
2021-10-02 00:52:07 +0800

    removed the normalisation from heur

--- Lab1/src/Q3.py
+++ Lab1/src/Q3.py
@@ -1,39 +1,39 @@
 import json
 import math
+import timeit
 from queue import PriorityQueue
 
-f = open('Lab1\G.json',)
-f1 = open('Lab1\Cost.json')
-f2 = open('Lab1\Dist.json')
-f3 = open('Lab1\Coord.json')
+start = timeit.default_timer()
+f = open('G.json',)
+f1 = open('Cost.json')
+f2 = open('Dist.json')
+f3 = open('Coord.json')
 graph = json.load(f)
 energycost = json.load(f1)
 dist = json.load(f2)
 coord = json.load(f3)
 predecessor = {}
 
+def linear_heuristic(currentNode, endNode,distNext): #euclidean_distance
+  starting = coord[currentNode]
+  ending = coord[endNode]
+  euclidean_distance = math.sqrt((starting[0] - ending[0])**2 + (starting[1] - ending[1])**2)
+  return euclidean_distance #havent normalise yet
 
-def linear_heuristic(currentNode, endNode):  # euclidean_distance
-    starting = coord[currentNode]
-    ending = coord[endNode]
-    euclidean_distance = math.sqrt(
-        (starting[0] - ending[0])**2 + (starting[1] - ending[1])**2)
-    temp1 = str(endNode)+','+str(currentNode)
-    return euclidean_distance
-
-
-def updatedAStar(startnode, endnode):
+#not working likely because of stackoverflow
+def updatedAStar (startnode,endnode):
     visited = set()
     q = PriorityQueue()
     predecessor[startnode] = 0
     for neighbour in graph[startnode]:
         temp1 = str(neighbour)+','+str(startnode)
         totalenergy = energycost[temp1]
-        heurDist = linear_heuristic(startnode, endnode)/11
-        totaldist = dist[temp1] + heurDist
-        q.put((totaldist, (startnode, neighbour), totalenergy, heurDist))
+        distNext = dist[temp1]
+        heurDist = linear_heuristic(startnode,endnode, distNext)
+        totaldist = distNext +heurDist
+        q.put((totaldist,(startnode,neighbour),totalenergy,heurDist))
     while q:
-        traveldist, (predecessornode, current), energy, hdist = q.get()
+        traveldist,(predecessornode,current),energy,hdist = q.get()
         if current not in visited:
             visited.add(current)
             predecessor[current] = predecessornode
@@ -42,38 +42,32 @@ def updatedAStar(startnode, endnode):
             for neighbours in graph[current]:
                 temp = str(neighbours)+','+str(current)
                 totalenergy = energy + energycost[temp]
-                if neighbours not in visited and totalenergy <= 287932:
-                    heurDist = linear_heuristic(startnode, endnode)/11
-                    totaldist = dist[temp]+traveldist+heurDist-hdist
+                if neighbours not in visited and totalenergy<=287932:
+                    distNext = dist[temp]
+                    heurDist = linear_heuristic(startnode,endnode, distNext)
+                    totaldist = distNext+traveldist+heurDist-hdist
                     predecessor[neighbours] = current
-                    q.put((totaldist, (current, neighbours), totalenergy, heurDist))
+                    q.put((totaldist,(current,neighbours),totalenergy,heurDist))
+                    
 
-
-def printshortestpath(startnode, endnode):
+def printshortestpath(startnode,endnode):
     shortestpath = []
     shortestpath.append(endnode)
     movement = endnode
     while (predecessor[movement] != startnode):
         shortestpath.insert(0, predecessor[movement])
         movement = predecessor[movement]
-    shortestpath.insert(0, startnode)
-
-    # print("Shortest path length: " + str(len(shortestpath)))
-    totalDist = 0
-    energyCost = 0;
-    print("Shortest Path: \nS -> ", end = '')
+    shortestpath.insert(0,startnode)
+    #print("This is the length of the shortest path " +str(len(shortestpath)))
+    checkDist = 0
     for i in range(len(shortestpath)-1):
-        print(shortestpath[i]+" -> ", end = '')
-        a = shortestpath[i]
-        b = shortestpath[i+1]
-        temp1 = str(b)+','+str(a)
-        totalDist += dist[temp1]
-        energyCost += energycost[temp1]
-    print("T")
-    print("\nShortest Distance: %.2f" % round(totalDist, 2))
-    print("\nTotal Energy Cost: "+str(energyCost))
+      a = shortestpath[i]
+      b = shortestpath[i+1]
+      temp1 = str(b)+','+str(a)
+      checkDist+= dist[temp1]
+    #print("The total distance of this function is: "+str(checkDist))
 
-
-# ucs('1', '50')
-# updatedAStar('1', '50')
-# printshortestpath('1', '50')
+updatedAStar('1','50')
+printshortestpath('1','50')
+stop = timeit.default_timer()
+#print('Time: ', stop - start)
