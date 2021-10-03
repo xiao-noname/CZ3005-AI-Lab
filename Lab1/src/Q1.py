39a650c866a47d27092d17b0c7f5909789853ac7
queklinhui12@gmail.com <queklinhui12@gmail.com>
2021-10-03 12:14:43 +0800

    changed question 1 to ucs

--- Lab1/src/Q1.py
+++ Lab1/src/Q1.py
@@ -1,5 +1,6 @@
 import json
 import timeit
+from queue import PriorityQueue
 
 f = open('Lab1\G.json',)
 f1 = open('Lab1\Cost.json')
@@ -8,54 +9,59 @@ graph = json.load(f)
 energycost = json.load(f1)
 dist = json.load(f2)
 
-visited = []
-queue = []
 predecessor = {}
 
-def bfs(startnode, endnode):
-    start_bfs = timeit.default_timer()
-    visited.append(startnode)
-    queue.append(startnode)
-    predecessor[startnode]= [0,0]
-    while queue:
-        m = queue.pop(0)
-        for neighbour in graph[m]:
-            if neighbour not in visited:
-                visited.append(neighbour)
-                queue.append(neighbour)
-                temp = str(m)+','+str(neighbour)
-                tempEnergy = energycost[temp]
-                tempdist = dist[temp]
-                predecessor[neighbour] = [m,tempdist]
-                if neighbour == endnode:
-                    stop_bfs = timeit.default_timer()
-                    printshortestpath(endnode)
-                    print('Time: ', stop_bfs - start_bfs)
-                    return
+
+def ucs_noenergy(startnode, endnode):
+    start_ucs = timeit.default_timer()
+    visited = set()
+    q = PriorityQueue()
+    predecessor[startnode] = 0
+    for neighbour in graph[startnode]:
+        temp1 = str(neighbour)+','+str(startnode)
+        totalenergy = energycost[temp1]
+        totaldist = dist[temp1]
+        q.put((totaldist, (startnode, neighbour), totalenergy))
+    while q:
+        traveldist, (predecessornode, current), energy = q.get()
+        if current not in visited:
+            visited.add(current)
+            predecessor[current] = predecessornode
+            if current == endnode:
+                stop_ucs = timeit.default_timer()
+                printshortestpath(startnode, endnode)
+                print('Time: ', stop_ucs - start_ucs)
+                return
+            for neighbours in graph[current]:
+                temp = str(neighbours)+','+str(current)
+                totalenergy = energy + energycost[temp]
+                if neighbours not in visited:
+                    totaldist = dist[temp]+traveldist
+                    predecessor[neighbours] = current
+                    q.put((totaldist, (current, neighbours), totalenergy))
 
 
-def printshortestpath(endnode):
+def printshortestpath(startnode, endnode):
     shortestpath = []
     shortestpath.append(endnode)
     movement = endnode
-    totaldist = 0
-    while (predecessor[movement][0] != 0):
-        shortestpath.insert(0, predecessor[movement][0])
-        totaldist += predecessor[movement][1]
-        movement = predecessor[movement][0]
+    while (predecessor[movement] != startnode):
+        shortestpath.insert(0, predecessor[movement])
+        movement = predecessor[movement]
+    shortestpath.insert(0, startnode)
 
-    checkDist = 0
-    energyCost = 0;
-    print("Shortest Path: \nS -> ", end = '')
+    totalDist = 0
+    energyCost = 0
+    print("Shortest Path: \nS -> ", end='')
     for i in range(len(shortestpath)-1):
-        print(shortestpath[i]+" -> ", end = '')
+        print(shortestpath[i]+" -> ", end='')
         a = shortestpath[i]
         b = shortestpath[i+1]
         temp1 = str(b)+','+str(a)
-        checkDist += dist[temp1]
+        totalDist += dist[temp1]
         energyCost += energycost[temp1]
     print(endnode+" -> T")
-    print("\nShortest Distance: %.2f" % round(totaldist, 2))
+    print("\nShortest Distance: %.2f" % round(totalDist, 2))
     print("Total Energy Cost: "+str(energyCost))
 
-# bfs('1','50')
+# ucs('1','50')
--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -3,12 +3,10 @@ import Q2
 import Q3
 
 print("|| Task 1: Breadth First Search ||")
-Q1.bfs('1','50')
+Q1.ucs_noenergy('1', '50')
 
 print("\n|| Task 2: Uniform Cost Search ||\n")
-Q2.ucs('1','50')
+Q2.ucs('1', '50')
 
 print("\n|| Task 3: A* Search ||\n")
 Q3.updatedAStar('1', '50')
-
-
