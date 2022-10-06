e0bccbe6f9bf1e1d11881cd7dbdb4eebadbfc827
010101_010101 <xiao0121@e.ntu.edu.sg>
2022-10-06 14:43:15 +0800

    Update Q3.py

--- Lab1/src/Q3.py
+++ Lab1/src/Q3.py
@@ -7,11 +7,11 @@ f = open('/Users/xiaolingyi/Documents/GitHub/CZ3005-AI-Lab/Lab1/G.json',)
 f1 = open('/Users/xiaolingyi/Documents/GitHub/CZ3005-AI-Lab/Lab1/Cost.json')
 f2 = open('/Users/xiaolingyi/Documents/GitHub/CZ3005-AI-Lab/Lab1/Dist.json')
 f3 = open('/Users/xiaolingyi/Documents/GitHub/CZ3005-AI-Lab/Lab1/Coord.json')
-graph = json.load(f)
-energycost = json.load(f1)
-dist = json.load(f2)
-coord = json.load(f3)
-predecessor = {}
+graph = json.load(f) # an adjacency list where the neighbor list of node ‘v’ 
+energycost = json.load(f1)# energy cost between a pair of node (v, w)
+dist = json.load(f2)# distance between a pair of node (v, w) 
+coord = json.load(f3)# coordination of a node ‘v’ is a pair (X, Y) 
+predecessor = {} # dictionary
 
 def linear_heuristic(currentNode, endNode):
     starting_x = coord[currentNode][0]
@@ -30,31 +30,31 @@ def updatedAStar (startnode,endnode,weight):
     predecessor[startnode] = 0
     for neighbour in graph[startnode]: # starting from startnode
         temp1 = str(neighbour)+','+str(startnode)
-        totalenergy = energycost[temp1]
-        distNext = dist[temp1]
-        heurDist = linear_heuristic(neighbour, endnode)
+        totalenergy = energycost[temp1] # geting energy cost between neighbor and startnode
+        distNext = dist[temp1] #the distance traveled so far
+        heurDist = linear_heuristic(neighbour, endnode)#what is the eulidean distance of the node to endnode
         functionDist = distNext + heurDist
-        q.put((functionDist, (startnode, neighbour), totalenergy, distNext))
+        q.put((functionDist, (startnode, neighbour), totalenergy, distNext)) # tuple comparison, functionDist is compaired first
     while q: # while the priority Queue is not exhaustive
         functionDist, (predecessornode, current), energy, traveldist = q.get()
         if current not in visited:  #if the new node have not been visit before
-            predecessor[current] = predecessornode
+            predecessor[current] = predecessornode # store in dictionary as "current" : predecessornode
             if current == endnode: # find the shortest path
                 stop_astar = timeit.default_timer()
                 printshortestpath(startnode, endnode)
                 print('Time: ', stop_astar - start_astar)
                 return
-            for neighbours in graph[current]:
+            for neighbours in graph[current]: # loop though the an adjacency list of current
                 temp = str(neighbours)+','+str(current)
                 totalenergy = energy + energycost[temp]
-                if neighbours not in visited and totalenergy <= 287932:
+                if neighbours not in visited and totalenergy <= 287932: # energy budget
                     visited.add(current)
                     distNext = dist[temp]
-                    heurDist = linear_heuristic(neighbours, endnode)
-                    gdist = traveldist + distNext
-                    if gdist < heurDist:
-                        fdist = gdist + heurDist
-                    else:
+                    heurDist = linear_heuristic(neighbours, endnode)#what is the eulidean distance of the node to endnode
+                    gdist = traveldist + distNext # the distance traveled so far
+                    if gdist < heurDist: # if the distance traveled so far smaller then the estimated reaching distance( less then half way)
+                        fdist = gdist + heurDist # functionalDist = distance traveled so far + huristic
+                    else: # if nearing to rinding the end goal
                         fdist = (gdist+(2*weight-1)*heurDist)/weight
                     q.put((fdist, (current, neighbours), totalenergy, gdist))
 
