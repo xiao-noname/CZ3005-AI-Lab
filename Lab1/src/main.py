baf3a6d43e403579cb97c8369f136e5f5f28ace7
MaGH <mgh1208.29@gmail.com>
2022-10-07 11:37:54 +0800

    update main

--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -3,7 +3,7 @@ import Q2
 import Q3
 
 print("|| Task 1: Breadth First Search ||")
-Q1.ucs_noenergy('1', '50')
+Q1.Task1('1', '50')
 
 print("\n|| Task 2: Uniform Cost Search ||\n")
 Q2.ucs('1', '50')
